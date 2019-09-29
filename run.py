3a522deecaada22057f840b971a3c2d30e735469
gejun-siemens <gejun318@sina.com>
2019-09-29 13:25:45 +0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#4.Flask应用程序实例的run方法,启动WEB服务器
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000,debug=True)
